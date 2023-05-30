0e6e886ebc15e90dc7efc3b0f5116cf551bd98f9
Paul Daniel Faria <Nashenas88@users.noreply.github.com>
2023-05-29 19:34:08 -0700

    Add initial idea for calibration data repo

---
+++ Android/app/src/main/java/com/mechaferris/mechaferris/utils/Mutex.kt
@@ -0,0 +1,27 @@
+package com.mechaferris.mechaferris.utils
+
+import kotlinx.coroutines.sync.Mutex as KMutex
+
+class Mutex<T>(private var value: T) {
+    private val mutex: KMutex = KMutex(locked = false)
+
+    suspend fun lock(): MutexGuard<T> {
+        mutex.lock()
+        return MutexGuard(mutex, value)
+    }
+
+    suspend fun <R>withLock(accessor: suspend (T) -> R): R {
+        mutex.lock()
+        try {
+            return accessor(value)
+        } finally {
+            mutex.unlock()
+        }
+    }
+}
+
+class MutexGuard<T>(private val mutex: KMutex, var value: T) {
+    fun finalize() {
+        mutex.unlock()
+    }
+}
